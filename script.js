fdaeb9ad1d1f345326dbc305b87a827f952acd23
Gianpierre533 <gianpierrevasquez7@gmail.com>
2026-05-11 22:00:07 -0500

    feat:interfaz HTML y primera funcion

--- script.js
+++ script.js
@@ -1,34 +1,17 @@
-let edad = prompt("¿Cuántos años tienes?");
-edad = Number(edad);
+const inputIntento = document.getElementById('inputIntento');
+const btnAdivinar = document.getElementById('btnAdivinar');
+const mensaje = document.getElementById('mensaje');
+const contador = document.getElementById('contador');
+const historial = document.getElementById('historial');
+const btnReiniciar = document.getElementById('btnReiniciar');
+const tarjeta = document.getElementById('game-card');
 
-if (edad >= 18) {
-  alert("Puedes votar 🗳️");
-} else {
-  alert("Aún no puedes votar 🧒");
-}
-let temperatura = Number(prompt("¿Qué temperatura hace hoy?"));
+console.log('Elementos conectados:', inputIntento, btnAdivinar, mensaje, contador, historial, btnReiniciar, tarjeta);
 
-if (temperatura < 15) {
-  alert("Hace frío, ponte abrigo 🧥");
-} else if (temperatura <= 25) {
-  alert("Clima templado, usa casaca ligera 🧢");
-} else {
-  alert("Hace calor, usa polo y gorra ☀️");
-}
-let clave = prompt("Crea una contraseña:");
-if (clave.length >= 6) {
-  alert("Contraseña válida ✅");
-} else {
-  alert("Contraseña muy corta ❌");
-}
-if (edad < 13) {
-  alert("Eres un niño 🧒");
-} else if (edad <= 17) {
-  alert("Eres un adolescente 👦");
-} else {
-  alert("Eres un adulto 👨‍🦱");
+function mostrarMensaje(texto, color) {
+  mensaje.textContent = texto;
+  mensaje.style.color = color;
 }
 
-if (edad > 18 && temperatura < 15) {
-  alert("Adulto con frío: lleva abrigo extra 🧣");
-}
+// Prueba la función
+mostrarMensaje('¡Bienvenido al juego!', '#e94560');
